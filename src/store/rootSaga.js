9a0b7ca28c5813e0c4588d8f30c3cbb8d129299b
Charles NDAYISABA <nccharles1@gmail.com>
2021-10-13 11:36:17 +0200

    correct path

--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
-import ContactSaga from './contacts/saga';
-import AppointmentSaga from './appointments/saga';
+import ContactSaga from './contact/saga';
+import AppointmentSaga from './appointment/saga';
 
 export default function* rootSaga() {
     yield all([
