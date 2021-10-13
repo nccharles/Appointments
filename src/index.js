70332148add16d6f98f69966fde08aadbb217c27
Charles NDAYISABA <nccharles1@gmail.com>
2021-10-13 09:54:26 +0200

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from 'react-redux'
+import store from './store'
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
+  <Provider store={store}>
   <Router>
     <App />
-  </Router>,
+  </Router>
+  </Provider>,
   document.getElementById('root')
 );
