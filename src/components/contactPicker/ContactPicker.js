14599e35aa18624457d9487c2d7addde48ca6c2f
Charles NDAYISABA <nccharles1@gmail.com>
2021-10-17 18:09:22 +0200

    refactoring

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { removeContact } from "../../store/contact/action";
-const ContactPicker = ({contacts,onChange}) => {
+const ContactPicker = ({ contacts, onChange }) => {
   const options = contacts.map((contact) => (
     <option value={contact.id}>{contact.name}</option>
   ));
 
-  return <select onChange={onChange} name="contact">
-    <option value={0}>Select...</option>
-  {options}</select>;
+  return (
+    <select onChange={onChange} name="contact">
+      <option value={0}>Select...</option>
+      {options}
+    </select>
+  );
 };
 
 export default ContactPicker;
